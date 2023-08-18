09d4b0642a50e17c5874f5ed0ba82e7aba028982
Manasseh Gitau <kimmanasseh14@gmail.com>
2023-08-18 18:32:57 +0300

    add watering functionality

--- Firmware/board Firmware/nodemcuv2-Firmware/src/main.cpp
+++ Firmware/board Firmware/nodemcuv2-Firmware/src/main.cpp
@@ -1,18 +1,64 @@
 #include <Arduino.h>
+#include <cassert>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TEST_PROTOTYPE
+
+const int pumpRelayPin {D2};
+const int soilMoisturePin {A0};
+const int tooWetSoil{500};
+const int tooDrySoil{300};
+
+// function prototypes
+void waterPlant(int, int);
+void notWaterPlant(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(pumpRelayPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int soilMoistureValue {analogRead(soilMoisturePin)};
+  #ifdef TEST_PROTOTYPE
+  Serial.print("Soil Moisture: ");
+  Serial.println(soilMoistureValue);
+  #endif
+
+  if (soilMoistureValue > tooWetSoil) 
+  {
+    notWaterPlant(soilMoistureValue, tooWetSoil);
+    delay(300);
+  } 
+  else if (soilMoistureValue < tooDrySoil) 
+  {
+    waterPlant(soilMoistureValue , tooWetSoil);
+    delay(300);
+  } 
+  else 
+  {
+    #ifdef TEST_PROTOTYPE
+    Serial.println("Moisture is OK, don't water");
+    #endif
+    notWaterPlant(soilMoistureValue, tooDrySoil);
+  }
+
+  delay(300);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void waterPlant(int soilMoistureValue, int tooWetSoil)
+{
+  assert(soilMoistureValue < tooDrySoil); // safety feature(confirmation)
+  #ifdef TEST_PROTOTYPE
+  Serial.println("Too dry, water");
+  #endif
+  digitalWrite(pumpRelayPin, HIGH);
+}
+
+void notWaterPlant(int soilMoistureValue, int tooWetSoil)
+{
+  assert(soilMoistureValue > tooWetSoil);
+  #ifdef TEST_PROTOTYPE
+  Serial.println("Too wet, don't water");
+  #endif
+  digitalWrite(pumpRelayPin, LOW);
 }
